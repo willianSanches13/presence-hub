83754283f6e31305d968469cca1d499aa14365b7
willian.sanches <willian.sanches@avenuecode.com>
2024-11-10 15:30:40 -0300

    fix: configuring back and front repos

---
+++ backend/presence-hub-back/src/main/java/com/oficina/presence_hub/PresenceHubApplication.java
@@ -0,0 +1,15 @@
+package com.oficina.presence_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresenceHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresenceHubApplication.class, args);
+
+		//teste
+	}
+
+}
---
+++ backend/presence-hub-back/src/test/java/com/oficina/presence_hub/PresenceHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oficina.presence_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresenceHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
